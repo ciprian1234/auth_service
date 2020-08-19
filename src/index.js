f4e649d156cdb8d1df408b6d7832bf706b9286c9
Ciprian Andrei <andrei.stefanel.ciprian@gmail.com>
2020-08-19 18:17:07 +0300

    added express and dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("ok");
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("ok");
+});
+
+app.listen(process.env["PORT"], () =>
+  console.log(`Server is listening at: http://localhost:${process.env["PORT"]}/`)
+);
